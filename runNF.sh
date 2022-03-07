2588782deb5f772e902e8dc3f2c504461f02b41a
ferygood <yaochung41@gmail.com>
2022-03-08 00:06:10 +0100

    change mem size

--- runNF.sh
+++ runNF.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=nowick
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
+#SBATCH --mem=750M
 #SBATCH --time=10-00:00:00
 #SBATCH --mail-type=end
 #SBATCH --mail-user=yaochung41@gmail.com
